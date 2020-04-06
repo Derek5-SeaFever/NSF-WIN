cdaf57c431ff0362128c2a498c01130437242c9b
Populus Euphratica <58065746+Derek5-SeaFever@users.noreply.github.com>
2020-04-06 15:26:53 +0800

    Add files via upload

---
+++ Batch-resource/bin/use/ulist.bat
@@ -0,0 +1,12 @@
+cls
+title NPM¾µÏñÁÐ±í
+echo [33m===MirrorList===[37m
+echo [35m1.[32m npm [35m     ---   [36mhttps://registry.npmjs.org/[37m
+echo [35m2.[32m cnpm [35m    ---   [36mhttp://r.cnpmjs.org/[37m
+echo [35m3.[32m taobao [35m  ---   [36mhttps://registry.npm.taobao.org/[37m
+echo [35m4.[32m nj [35m      ---   [36mhttps://registry.nodejitsu.com/[37m
+echo [35m5.[32m rednpm [35m  ---   [36mhttp://registry.mirror.cqupt.edu.cn/[37m
+echo [35m6.[32m npmMirror[35m---   [36mhttps://skimdb.npmjs.com/registry/[37m
+echo [35m7.[32m edunpm [35m  ---   [36mhttp://registry.enpmjs.org/[37m
+echo [35m8.[32m huawei [35m  ---   [36mhttps://mirrors.huaweicloud.com/repository/npm/[37m
+echo [33m===============[37m
