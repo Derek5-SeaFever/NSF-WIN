512b076d129549984098d8417c738e24819f2c4e
Populus Euphratica <58065746+Derek5-SeaFever@users.noreply.github.com>
2020-04-06 15:26:27 +0800

    Create uhelp.bat

---
+++ Batch-resource/bin/use/uhelp.bat
@@ -0,0 +1,14 @@
+cls
+title USE控制台帮助文档
+echo [33m===帮助文档===[37m
+echo [35m1. [37m当显示
+echo       "Which mirror will you choose?"
+echo       "Your decision is:" 时，
+echo [31m   直接输入镜像名[37m即可！
+
+echo [35m2. [37m如果不清楚有哪些镜像可以使用，可以输入[36m[ list \ ls ][37m即可查看清单列表。
+
+echo [35m3. [37m如果需要再次查看本文档可以输入 [36m[ help ][37m即可再次查看本文档。
+
+echo [35m4. [37m退出USE控制台请输入[36m [exit ^| close ^| nsf] [37m。
+echo [33m=================[37m
