35d49c27ecebfe05b7797df46c4b5d435cb9fded
Populus Euphratica <58065746+Derek5-SeaFever@users.noreply.github.com>
2020-04-06 15:25:27 +0800

    Add files via upload

---
+++ Batch-resource/bin/rednpm.bat
@@ -0,0 +1,5 @@
+@echo off
+echo [33m+------------+[37m
+echo [33m^| [31m¸ü¸Ä³É¹¦£¡ [33m^|^>^>[36mrednpm[37m
+echo [33m+------------+[37m
+npm config set registry http://registry.mirror.cqupt.edu.cn/
---
+++ Batch-resource/bin/showMirror.bat
@@ -0,0 +1,2 @@
+echo µ±Ç°ÕýÔÚÊ¹ÓÃ¾µÏñµØÖ·:[34m
+npm config get registry & echo [37m
